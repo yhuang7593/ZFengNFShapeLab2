e235c6eb816fd5396da27f1ee78bb28c7a8341c3
zfeng7527 <zfeng7527@bths.edu>
2018-10-16 08:42:47 -0400

    Hello

--- src/ZhiFeng/Main.java
+++ src/ZhiFeng/Main.java
@@ -3,6 +3,12 @@ package ZhiFeng;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.primtln("xd");
+        Shape shape1 = new Rectangle();
+        Shape shape2 = new ETriangle();
+        Shape shape3 = new Circle();
+        Shape shape4 = RPentagon();
+        System.out.println(shape4);
+        System.out.println("My primeter is: "+shape4.getPrimeter());
+        System.out.println("My Area is: "+ shape4.getArea());
     }
 }

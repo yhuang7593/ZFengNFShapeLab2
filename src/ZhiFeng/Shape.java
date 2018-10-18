9d80f1e3d5e1cbe33e7e743d73e280ffb4a81b6b
zfeng7527 <zfeng7527@bths.edu>
2018-10-18 08:11:54 -0400

    Today

---
+++ src/ZhiFeng/Shape.java
@@ -0,0 +1,15 @@
+package ZhiFeng;
+
+public abstract class Shape {
+    private String type;
+    public Shape(String type){
+        this.type = type;
+    }
+    public abstract double getArea();
+    public abstract double getPerimeter();
+    public String toString(){
+        return "I am a " + type +".";
+    }
+}
+
+
